474bd0a616f5dd8d6444d3d1ae79371bdbd8a816
Ross <144515088+RossAngel@users.noreply.github.com>
2024-11-02 09:30:29 +0530

    Add files via upload

---
+++ todolist/login.js
@@ -0,0 +1,15 @@
+$('#formbox').submit(function(event) {
+    event.preventDefault();
+    
+    var username = $('#username').val();
+    var password = $('#password').val();
+  
+    // Check credentials
+    if (username === "admin" && password === "12345") {
+      // Redirect to main page
+      window.location.href = "fsd todo.html";
+    } else {
+      alert("Invalid username or password");
+    }
+  });
+  
